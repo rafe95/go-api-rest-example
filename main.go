ccdc442f225445ff8b543cf6ba78de71a0cd3372
Rafe <rafe.estevez@gmail.com>
2020-01-09 21:15:50 -0500

    [ADD] Laptop model

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import "github.com/jinzhu/gorm"
 var db *gorm.DB
 var err error
 
+type Laptop struct {
+	ID    int    `json:"id"`
+	Brand string `json:"brand"`
+	Model string `json:"model"`
+}
+
 func main() {
 
 	const CnString = "host=localhost port=5432 user=gorm dbname=person_db sslmode=disable password=gormPassword"
